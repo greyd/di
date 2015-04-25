8302c895fbeeb6ccd3009ef96059bb2243d0635d
Valerii Pavlov <pavlov.valerii@gmail.com>
2015-04-25 15:19:45 +0300

    Check gulpfile codestule

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
+'use strict';
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var jasmine = require('gulp-jasmine');
-var istanbul = require('gulp-istanbul');
+var gulpJshint = require('gulp-jshint');
+var gulpJasmine = require('gulp-jasmine');
+var gulpIstanbul = require('gulp-istanbul');
 
 gulp.task('jshint', function() {
-    return gulp.src(['./{specs, src}/*.js', 'gulpfile'])
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+    return gulp.src(['./{specs, src}/*.js', 'gulpfile.js'])
+        .pipe(gulpJshint())
+        .pipe(gulpJshint.reporter('default'));
 });
 
 gulp.task('test', function (cb) {
     gulp.src(['src/**/*.js'])
-        .pipe(istanbul())
-        .pipe(istanbul.hookRequire())
+        .pipe(gulpIstanbul())
+        .pipe(gulpIstanbul.hookRequire())
         .on('finish', function () {
             gulp.src(['specs/*.js'])
-                .pipe(jasmine())
-                .pipe(istanbul.writeReports())
+                .pipe(gulpJasmine())
+                .pipe(gulpIstanbul.writeReports())
                 .on('end', cb);
         });
 });
