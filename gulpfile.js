aa476ad6cd5911c71259d4e8bce4b911c282b2a8
Valerii Pavlov <pavlov.valerii@gmail.com>
2015-04-25 16:00:57 +0300

    Create a TDD gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,40 @@
 'use strict';
+var _ = require('lodash');
 var gulp = require('gulp');
 var gulpJshint = require('gulp-jshint');
 var gulpJasmine = require('gulp-jasmine');
 var gulpIstanbul = require('gulp-istanbul');
+var pathConf = {
+    src: './src/**/*.js',
+    specs: './specs/**/*.js',
+    gulpfile: 'gulpfile.js'
+};
 
 gulp.task('jshint', function() {
-    return gulp.src(['./{specs, src}/*.js', 'gulpfile.js'])
+    var src = _(pathConf)
+        .pick('src', 'specs', 'gulpfile')
+        .values()
+        .value();
+    return gulp.src(_.values())
         .pipe(gulpJshint())
         .pipe(gulpJshint.reporter('default'));
 });
 
-gulp.task('test', function (cb) {
-    gulp.src(['src/**/*.js'])
+gulp.task('test', function () {
+    return gulp.src(pathConf.specs)
+        .pipe(gulpJasmine());
+});
+
+gulp.task('tdd', function () {
+    gulp.watch(pathConf.specs, ['test']);
+});
+
+gulp.task('test-report', function (cb) {
+    gulp.src(pathConf.src)
         .pipe(gulpIstanbul())
         .pipe(gulpIstanbul.hookRequire())
         .on('finish', function () {
-            gulp.src(['specs/*.js'])
+            gulp.src(pathConf.specs)
                 .pipe(gulpJasmine())
                 .pipe(gulpIstanbul.writeReports())
                 .on('end', cb);
